a1dd2e15520e068f9f18caa2ec02d92b6387db1f
Madhav <madhavpaliwal02@gmail.com>
2023-08-29 14:02:35 +0530

    File Upload : File storing & retreiving

--- file_upload/src/main/java/com/example/file_upload/controller/FileCtrl.java
+++ file_upload/src/main/java/com/example/file_upload/controller/FileCtrl.java
@@ -1,10 +1,15 @@
 package com.example.file_upload.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.file_upload.payload.FileResponse;
 import com.example.file_upload.service.FileService;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -26,6 +32,7 @@ public class FileCtrl {
     @Value("${project.image}")
     public String path;
 
+    // Upload image
     @PostMapping("/upload")
     public ResponseEntity<FileResponse> uploadImageHandler(@RequestParam("image") MultipartFile image) {
         String fileName = null;
@@ -37,5 +44,14 @@ public class FileCtrl {
                     HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(new FileResponse(fileName, "Image uploaded successfully !!"), HttpStatus.OK);
+    }
+
+    // Get Image
+    @GetMapping(value = "/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void getResourceHandler(@PathVariable("imageName") String imageName,
+            HttpServletResponse response) throws IOException {
+        InputStream resource = this.fileService.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
     }
 }
--- file_upload/src/main/java/com/example/file_upload/service/FileServiceImpl.java
+++ file_upload/src/main/java/com/example/file_upload/service/FileServiceImpl.java
@@ -1,9 +1,13 @@
 package com.example.file_upload.service;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,8 +21,19 @@ public class FileServiceImpl implements FileService {
         // File name
         String name = file.getOriginalFilename();
 
+        // Providing unique random path
+        String randomID = UUID.randomUUID().toString();
+        String ext = name.substring(name.lastIndexOf("."));
+
+        // Validating the image format
+        if (!(ext.equalsIgnoreCase(".png") || ext.equalsIgnoreCase(".jpg") || ext.equalsIgnoreCase(".jpeg")))
+            throw new IllegalArgumentException("File name is invalid");
+
+        System.out.println("EXt: " + ext);
+        String newName = randomID.concat(ext);
+
         // Full path
-        String filePath = path + File.separator + name;
+        String filePath = path + File.separator + newName;
 
         // Create folder if not created
         File f = new File(path);
@@ -28,7 +43,14 @@ public class FileServiceImpl implements FileService {
         // Save file in
         Files.copy(file.getInputStream(), Paths.get(filePath));
 
-        return name;
+        return newName;
+    }
+
+    @Override
+    public InputStream getResource(String path, String fileName) throws FileNotFoundException {
+        String fullPath = path + File.separator + fileName;
+        InputStream resource = new FileInputStream(fullPath);
+        return resource;
     }
 
 }
